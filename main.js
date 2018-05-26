6cb1ae5f22fbc901520c0ee45604d3549984fbb8
Jake Neild <jakeneildfb@yahoo.com>
2018-05-25 19:00:58 -0500

    Three simple exercises

---
+++ main.js
@@ -0,0 +1,63 @@
+/*const cookies = ["Oatmeal Raisin", "Chocolate Chip", "Sugar", "Peanut Butter", "Snickerdoodle", "Ginger"]
+
+for (let x = 0; x < cookies.length; x++) {
+    const currentCookie = cookies[x];
+    console.log(`Mmmmmmm... that's a good ${currentCookie} cookie`);
+} */
+
+
+
+/*function conjunction (firstWord, secondWord) {
+    const conjoinedWord = `${firstWord} ${secondWord}`;
+    return conjoinedWord;
+}
+
+console.log(conjunction("Master", "Card"));*/
+
+
+
+/*const ModSquad = {
+    "members": ["Pete Cochran", "Linc Hayes", "Julie Barnes", "Capt. Adam Greer", "Chief Barney Metcalf"],
+    "series": {
+        "start": "1968",
+        "end": "1973"
+    }
+}
+
+var HTMLRepresentation = `<h1>The Mod Squad</h1>`;
+
+ModSquad.members.forEach(member => {
+    HTMLRepresentation += `<div>${member}</div>`;
+});
+
+document.getElementsByClassName("show-info").innerHTML = HTMLRepresentation;*/
+
+/*const locations = [[1,1], [1,2], [1,3], [2,1], [2,2], [2,3]];
+
+var k = 0;
+
+for (k = 0; k < locations.length; k++) {
+    const currentLocation = locations[k];
+
+    var invalidLocation = false;
+
+    if (currentLocation[0] > 2) {
+        invalidLocation = true;
+    }
+
+    if (invalidLocation) {
+        console.log("This location is invalid");
+    }
+}
+
+console.log(`There were ${k} locations displayed`)*/
+
+/*function llamaNamer() {
+    const possibleNames = ["Larry", "Leon", "Leona", "Les", "Laura", "Lemony", "Lars", "Lekisha"];
+    var randomizer = Math.floor(Math.random() * 7);
+    var suffix = " the Llama"
+    var name = possibleNames[randomizer]
+    return name + suffix;
+}
+
+console.log(llamaNamer());*/
